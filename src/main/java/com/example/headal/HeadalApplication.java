8af92b8541792c58a4fd1d90903b7f7bfb93b654
jumdo12 <126929413+jumdo12@users.noreply.github.com>
2024-10-03 07:54:57 +0900

    Add files via upload

--- src/main/java/com/example/headal/HeadalApplication.java
+++ src/main/java/com/example/headal/HeadalApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HeadalApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HeadalApplication.class, args);
 	}
-
 }
